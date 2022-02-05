6a34217533581d3d0b134d42df25cd39e55db924
muhammadsaad91 <saadattari91@gmail.com>
2022-02-05 17:58:09 +0500

    ToDo list react

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,43 @@ import react, { useState } from "react";
 
 const App = () => {
     const[name , setName]=useState('');
-    const[clname,setClname]=useState('');
+    const[clname,setClname]=useState([]);
    
    
     const change=(e)=>{
         setName(e.target.value);
     }
 const clicked=()=>{
-    setClname(name)   
+    setClname( (olditems)=>{
+    return [...olditems,name];
+    } )   
+    setName('');
 }
+
 return (
     <>
     <div className="Main-div">
         <div className="inner-div">
            <div className="header"> <h1>ToDo List</h1></div>
        <div className="flex-inp">  
-        <div className="input-div"><input type="text" placeholder="Enter Item Name" onChange={change}/></div>
+        <div className="input-div"><input type="text" placeholder="Enter Item Name" value={name} onChange={change}/></div>
         <button onClick={clicked}>+</button>
         </div> 
-        <div className="list-div"><button>-</button><p>{clname}</p></div>
+       {clname.map( (itemval,index)=>{
+           return(
+           <div className="list-div">
+               <button  
+               onClick={
+                     ()=>{
+                        setClname( (old)=>{
+                            return old.filter( (val,id)=>{
+                                return id!==index;
+                            })
+                        })
+                     }
+               }
+            >-</button><p>{itemval}</p></div>
+       )})}
 
         </div>
     </div>
