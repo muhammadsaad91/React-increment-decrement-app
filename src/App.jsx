ddda924b5f6ff0d8e5c1f9220efa1df40598a6ae
muhammadsaad91 <saadattari91@gmail.com>
2022-02-05 13:59:06 +0500

    My project todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,30 @@
 import react, { useState } from "react";
 
-
-// jab bhi form lgaen ge tou form ki wjah se button click krne pe page auto refresh hoga 
-             // isko sahi kr skte hain 'event.preventDefault();' lga kr
-
-
 const App = () => {
-    const [fullName, setFullName] = useState({
-        fname: "",
-        lname: "",
-        email: '',
-        number:'',
+    const[name , setName]=useState('');
+    const[clname,setClname]=useState('');
+   
+   
+    const change=(e)=>{
+        setName(e.target.value);
+    }
+const clicked=()=>{
+    setClname(name)   
+}
+return (
+    <>
+    <div className="Main-div">
+        <div className="inner-div">
+           <div className="header"> <h1>ToDo List</h1></div>
+       <div className="flex-inp">  
+        <div className="input-div"><input type="text" placeholder="Enter Item Name" onChange={change}/></div>
+        <button onClick={clicked}>+</button>
+        </div> 
+        <div className="list-div"><button>-</button><p>{clname}</p></div>
 
-    });
-    const change = (event) => {
-        const { name, value } = event.target;
-        setFullName((prevState) => {
-            return {
-                ...prevState,
-                [name]: value,
-            };
-        });
-    };
-    const submit = (event) => {
-        // event.preventDefault();
-        console.log(fullName);
-    };
-
-
-
-
-    return (
-        <>
-            <form onSubmit={submit}>
-                    <h1>Full Name:{fullName.fname} {fullName.lname} </h1>
-                    <p>Email :{fullName.email}</p>
-                    <p>Number:{fullName.number}</p>
-                <div className="card">
-                    <input type="text" name="fname" value={fullName.fname} onChange={change}  placeholder="Enter Your first name"/>
-                    <input type="text" name="lname" value={fullName.lname} onChange={change}  placeholder="Enter Your last name"/>
-                    <input type="text" name="email" value={fullName.email} onChange={change} placeholder="Enter Your email" />          
-                    <input type="text" name="number" value={fullName.number} onChange={change} placeholder="Enter Your number" />
-                    <button type="Submit" >Submit Here</button>
-                </div>
-
-            </form>
-        </>
-    );
+        </div>
+    </div>
+    </>
+    )
 }
 export default App;
